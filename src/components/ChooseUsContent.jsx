9074fe02e1307a1ce55b97a7172b24c761be2297
Pancho <pancho.christian12@gmail.com>
2024-06-07 04:28:13 +0800

    Done with the Choose Us Page

--- src/components/ChooseUsContent.jsx
+++ src/components/ChooseUsContent.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import {
   motion,
   useMotionValue,
@@ -6,7 +6,10 @@ import {
   useInView,
   useScroll,
   useTransform,
+  easeInOut,
+  easeOut,
 } from "framer-motion";
+import { useWindowDimensions } from "./hooks/useWindowDimensions";
 import Tool from "../assets/Svg/ChooseUsIcon/colorTools.svg";
 import Customer from "../assets/Svg/ChooseUsIcon/colorReview.svg";
 import Shield from "../assets/Svg/ChooseUsIcon/colorShield.svg";
@@ -15,84 +18,278 @@ import Data from "../components/constants/chooseUsPage.json";
 
 const ChooseUsContent = () => {
   const divRef = useRef(null);
-  const divRef1 = useRef(null);
-  //   const isInView = useInView(divRef, {
-  //     margin: "0px 100px -50px 0px",
-  //   });
+  const { width } = useWindowDimensions();
+
+  console.log(width);
 
   const { scrollYProgress } = useScroll({
     target: divRef,
-    offset: ["start center", "end center"],
+    offset: ["start end", "end end"],
   });
 
-  const contentVariant = {
-    initial: { opacity: 1, scale: 0.5 },
-    animate: { opacity: 1, scale: 1 },
-  };
+  const [xPosition, setXPosition] = useState({ firstBox: 0, box: 0 });
 
-  const test = useTransform(scrollYProgress, [0, 0.25], [-1000, 0]);
-  const test1 = useTransform(scrollYProgress, [0.25, 0.5], [-1000, 0]);
-  const test2 = useTransform(scrollYProgress, [0.5, 0.75], [-1000, 0]);
-  const test3 = useTransform(scrollYProgress, [0.75, 1], [-1000, 0]);
+  useEffect(() => {
+    if (width >= 1280) {
+      setXPosition((prevPos) => ({
+        ...prevPos,
+        firstBox: (prevPos.firstBox = 0),
+        Box: (prevPos.box = -500),
+      }));
+    } else if (width < 1280) {
+      setXPosition((prevPos) => ({
+        ...prevPos,
+        firstBox: (prevPos.firstBox = -1200),
+        Box: (prevPos.box = -1200),
+      }));
+    }
+  }, [width]);
+
+  const sample = [0, 0.5];
+
+  // MOBILE
+  const firstBoxMobile = useTransform(
+    scrollYProgress,
+    [0, 0.4],
+    [xPosition.firstBox, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const secondBoxMobile = useTransform(
+    scrollYProgress,
+    [0.2, 0.6],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const thirdBoxMobile = useTransform(
+    scrollYProgress,
+    [0.4, 0.8],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const fourthBoxMobile = useTransform(
+    scrollYProgress,
+    [0.6, 1],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+
+  // TABLET
+  const firstBoxTablet = useTransform(
+    scrollYProgress,
+    [0, 0.4],
+    [xPosition.firstBox, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const secondBoxTablet = useTransform(
+    scrollYProgress,
+    [0.25, 0.5],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const thirdBoxTablet = useTransform(
+    scrollYProgress,
+    [0.45, 0.75],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const fourthBoxTablet = useTransform(
+    scrollYProgress,
+    [0.7, 0.9],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+
+  // Laptop
+  const firstBox = useTransform(
+    scrollYProgress,
+    [0, 0.4],
+    [xPosition.firstBox, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const secondBox = useTransform(
+    scrollYProgress,
+    [0.5, 0.65],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const thirdBox = useTransform(
+    scrollYProgress,
+    [0.6, 0.75],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+  const fourthBox = useTransform(
+    scrollYProgress,
+    [0.7, 0.85],
+    [xPosition.box, 0],
+    {
+      ease: easeInOut,
+    },
+  );
+
+  const test = useTransform(scrollYProgress, [0.7, 0.75], [0, 1]);
 
   return (
     <div
-      className="relative h-[200vh] overflow-hidden bg-adobe-white font-shareTech"
+      className="relative h-[250vh] justify-center bg-adobe-white font-shareTech lg:flex lg:flex-col xl:flex-row xl:items-center"
       ref={divRef}
     >
-      {/* {Data.map((service) => (
+      {/* FIRST HALF */}
+      <div className="h-1/2 lg:flex lg:h-1/3 xl:sticky xl:top-1/2 xl:z-20 xl:-translate-y-1/2">
+        {/* FIRST BOX */}
         <motion.div
-          key={service.id}
-          className="h-1/4 w-full border-b-4 border-solid border-black"
-          variants={contentVariant}
-          initial="initial"
-          animate="animate"
+          className="z-40 flex h-1/2 w-full flex-col items-center justify-center gap-10 border-b-2 border-r-2 border-solid border-adobe-gray bg-adobe-white px-12 text-center lg:grid lg:h-full lg:grid-cols-1 lg:grid-rows-2 lg:place-items-start lg:border-t-2 xl:border-t-0"
+          style={{
+            // translateX: firstBox,
+            translateX: width >= 1024 ? firstBoxTablet : firstBoxMobile,
+          }}
         >
-          <div>{service.title}</div>
-          <div>{service.description}</div>
-          <div
-            className="h-1/4 w-full bg-red-500"
-            style={{
-              backgroundImage: `url("${service.image}")`,
-              backgroundSize: "contain",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          ></div>
+          <div className="size-1/4 lg:row-start-1 lg:flex lg:size-full lg:place-content-center lg:place-items-center">
+            <div
+              className="size-full lg:size-2/5"
+              style={{
+                backgroundImage: `url("${Data[0].image}")`,
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+          </div>
+          <div className="flex max-w-[500px] flex-col gap-10 lg:relative lg:bottom-10 lg:row-start-2">
+            <div className="text-mobile-chooseUs-title h-[3rem] place-content-center rounded-bl-xl rounded-tr-xl border-2 border-solid border-black">
+              {Data[0].title}
+            </div>
+            <div className="text-mobile-chooseUs-description font-spaceMono">
+              {Data[0].description}
+            </div>
+          </div>
         </motion.div>
-      ))} */}
-      <motion.div
-        className="h-1/4 bg-orange-500"
-        style={{
-          translateX: test,
-        }}
-      >
-        TEST
-      </motion.div>
-      <motion.div
-        className="h-1/4 bg-red-500"
-        style={{
-          translateX: test1,
-        }}
-      >
-        TEST
-      </motion.div>
-      <motion.div
-        className="h-1/4 bg-green-500"
-        style={{
-          translateX: test2,
-        }}
-      >
-        TEST
-      </motion.div>
-      <motion.div
-        className="h-1/4 bg-blue-500"
-        style={{
-          translateX: test3,
-        }}
-      >
-        TEST
-      </motion.div>
+        {/* SECOND BOX */}
+        <motion.div
+          className="z-30 flex h-1/2 w-full flex-col items-center justify-center gap-10 border-b-2 border-solid border-adobe-gray bg-adobe-white px-12 text-center lg:grid lg:h-full lg:grid-cols-1 lg:grid-rows-2 lg:place-items-start lg:border-t-2 xl:border-r-2 xl:border-t-0"
+          style={{
+            translateX:
+              width <= 1023
+                ? secondBoxMobile
+                : width >= 1280
+                  ? secondBox
+                  : secondBoxTablet,
+          }}
+        >
+          <div className="size-1/4 lg:row-start-1 lg:flex lg:size-full lg:place-content-center lg:place-items-center">
+            <div
+              className="size-full lg:size-2/5"
+              style={{
+                backgroundImage: `url("${Data[1].image}")`,
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+          </div>
+          <div className="flex max-w-[500px] flex-col gap-10 lg:relative lg:bottom-10 lg:row-start-2">
+            <div className="text-mobile-chooseUs-title h-[3rem] place-content-center rounded-bl-xl rounded-tr-xl border-2 border-solid border-black">
+              {Data[1].title}
+            </div>
+            <div className="text-mobile-chooseUs-description font-spaceMono">
+              {Data[1].description}
+            </div>
+          </div>
+        </motion.div>
+      </div>
+      {/* SECOND HALF */}
+      <div className="h-1/2 lg:flex lg:h-1/3 lg:items-center xl:sticky xl:top-1/2 xl:z-10 xl:-translate-y-1/2">
+        {/* THIRD BOX */}
+        <motion.div
+          className="z-20 flex h-1/2 w-full flex-col items-center justify-center gap-10 border-b-2 border-solid border-adobe-gray bg-adobe-white px-12 text-center lg:grid lg:h-full lg:grid-cols-1 lg:grid-rows-2 lg:place-items-start lg:border-r-2"
+          style={{
+            // translateX: thirdBox,
+            translateX:
+              width <= 1023
+                ? thirdBoxMobile
+                : width >= 1280
+                  ? thirdBox
+                  : thirdBoxTablet,
+          }}
+        >
+          <div className="size-1/4 lg:row-start-1 lg:flex lg:size-full lg:place-content-center lg:place-items-center">
+            <div
+              className="size-full lg:size-2/5"
+              style={{
+                backgroundImage: `url("${Data[2].image}")`,
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+          </div>
+          <div className="flex max-w-[500px] flex-col gap-10 lg:relative lg:bottom-10 lg:row-start-2">
+            <div className="text-mobile-chooseUs-title h-[3rem] place-content-center rounded-bl-xl rounded-tr-xl border-2 border-solid border-black">
+              {Data[2].title}
+            </div>
+            <div className="text-mobile-chooseUs-description font-spaceMono">
+              {Data[2].description}
+            </div>
+          </div>
+        </motion.div>
+        {/* FOURTH BOX */}
+        <motion.div
+          className="z-10 flex h-1/2 w-full flex-col items-center justify-center gap-10 border-b-2 border-solid border-adobe-gray bg-adobe-white px-12 text-center lg:grid lg:h-full lg:grid-cols-1 lg:grid-rows-2 lg:place-items-start"
+          style={{
+            // translateX: fourthBox,
+            translateX:
+              width <= 1023
+                ? fourthBoxMobile
+                : width >= 1280
+                  ? fourthBox
+                  : fourthBoxTablet,
+
+            opacity: test,
+          }}
+        >
+          <div className="size-1/4 lg:row-start-1 lg:flex lg:size-full lg:place-content-center lg:place-items-center">
+            <div
+              className="size-full lg:size-2/5"
+              style={{
+                backgroundImage: `url("${Data[3].image}")`,
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            ></div>
+          </div>
+          <div className="flex max-w-[500px] flex-col gap-10 lg:relative lg:bottom-10 lg:row-start-2">
+            <div className="text-mobile-chooseUs-title h-[3rem] place-content-center rounded-bl-xl rounded-tr-xl border-2 border-solid border-black">
+              {Data[3].title}
+            </div>
+            <div className="text-mobile-chooseUs-description font-spaceMono">
+              {Data[3].description}
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 };
--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -7,12 +7,12 @@ const HeroPage = () => {
       {/* RED BORDER */}
       <div className="relative left-5 top-10 z-50 h-full w-full border-l-4 border-t-4 border-solid border-adobe-red"></div>
       {/* INNER WHITE BOX WRAPPER*/}
-      <div className="lg:grid-cols-6 lg:top-0 lg:left-0 absolute left-6 top-11 grid h-full w-full grid-cols-4 grid-rows-6 bg-adobe-white">
+      <div className="absolute left-6 top-11 grid h-full w-full grid-cols-4 grid-rows-6 bg-adobe-white lg:left-0 lg:top-0 lg:grid-cols-6">
         {/* PAGE IMAGE WRAPPER */}
-        <div className="lg:absolute lg:w-full lg:h-full lg:col-start-4 z-30 col-span-4 col-start-2 row-span-6 row-start-1 flex items-center">
+        <div className="z-30 col-span-4 col-start-2 row-span-6 row-start-1 flex items-center lg:absolute lg:col-start-4 lg:h-full lg:w-full">
           {/* PAGE IMAGE BLOCK */}
           <div
-            className="lg:h-full lg:mb-0 mb-5 h-[80%] w-full"
+            className="mb-5 h-[80%] w-full lg:mb-0 lg:h-full"
             style={{
               backgroundImage: `url(${Weld01})`,
               backgroundSize: "cover",
@@ -21,34 +21,36 @@ const HeroPage = () => {
           ></div>
         </div>
         {/*TOP RED BOX*/}
-        <div className="lg:hidden relative col-span-1 col-start-1 row-span-1 row-start-2 ml-auto h-full w-1/4 bg-adobe-red"></div>
+        <div className="absolute bottom-0 right-0 col-span-1 col-start-1 row-span-1 row-start-1 h-[55%] w-[10%] bg-adobe-red lg:hidden"></div>
+        <div className="absolute bottom-0 col-span-1 col-start-2 row-span-1 row-start-1 h-[55%] w-full bg-adobe-red lg:hidden"></div>{" "}
+        <div className="absolute right-0 top-0 col-span-1 col-start-1 row-span-1 row-start-2 h-[55%] w-[10%] bg-adobe-red lg:hidden"></div>
         {/* TITLE BOX */}
-        <div className="lg:col-start-1 lg:absolute lg:right-0 lg:w-[85%] lg:h-full lg:col-span-5 lg:row-start-2 lg:row-span-3 lg:pl-0 lg:bg-transparent z-40 col-span-3 col-start-1 row-span-2 row-start-3 flex flex-col justify-center bg-adobe-white bg-opacity-80 pl-[25%]">
-          <div className="text-mobile-title lg:justify-end lg:w-3/4 lg:flex-col lg:items-start md:items-center md:gap-2 max-md:flex-col max-md:justify-center md:basis-1/4 lg:basis-1/2 flex basis-1/2">
+        <div className="z-40 col-span-3 col-start-1 row-span-2 row-start-3 flex flex-col justify-center bg-adobe-white bg-opacity-80 pl-[25%] lg:absolute lg:right-0 lg:col-span-5 lg:col-start-1 lg:row-span-3 lg:row-start-2 lg:h-full lg:w-[85%] lg:bg-transparent lg:pl-0">
+          <div className="text-mobile-title flex basis-1/2 max-md:flex-col max-md:justify-center md:basis-1/4 md:items-center md:gap-2 lg:w-3/4 lg:basis-1/2 lg:flex-col lg:items-start lg:justify-end">
             <div>{Data.heroPageAries}</div>
             <div className="flex items-center">
               {Data.heroPageStainless}
-              <span className="max-lg:hidden relative left-5 text-5xl">
+              <span className="relative left-5 text-5xl max-lg:hidden">
                 and
               </span>
             </div>
           </div>
           <div className="max-lg:text-2xl lg:hidden">and</div>
-          <div className="text-mobile-title lg:basis-1/4 max-lg:w-3/4 flex basis-1/2 items-center bg-opacity-25">
+          <div className="text-mobile-title flex basis-1/2 items-center bg-opacity-25 max-lg:w-3/4 lg:basis-1/4">
             {Data.heroPageSubtitle}
           </div>
         </div>
-        <div className="lg:w-full lg:col-span-2 lg:col-start-4 lg:h-[37%] lg:absolute lg:top-[44%] lg:rounded-tr-3xl z-30 col-start-4 row-span-2 row-start-3 h-full w-1/2 bg-adobe-white bg-opacity-80 xs:w-1/4"></div>
+        <div className="z-30 col-start-4 row-span-2 row-start-3 h-full w-1/2 bg-adobe-white bg-opacity-80 xs:w-1/4 lg:absolute lg:top-[44%] lg:col-span-2 lg:col-start-4 lg:h-[37%] lg:w-full lg:rounded-tr-3xl"></div>
         {/* TAGLINE BOX */}
-        <div className="sm:w-1/4 lg:hidden z-40 col-start-4 row-span-1 row-start-5 h-8 w-1/2 rounded-br-xl bg-adobe-red xs:w-1/4"></div>
-        <div className="text-mobile-tagline lg:col-start-1 lg:absolute lg:right-11 lg:w-[75%] lg:h-1/4 lg:bottom-[1.15rem] lg:rounded-tl-xl lg:col-span-4 lg:row-start-4 lg:row-span-1 z-40 col-span-3 col-start-1 row-start-5 flex h-8 items-center justify-start bg-adobe-red pl-[20%] font-spaceMono text-adobe-white"></div>
-        <div className="text-mobile-tagline lg:col-start-1 lg:absolute lg:right-11 lg:w-[75%] lg:h-1/4 lg:bottom-[1.15rem] lg:justify-center lg:col-span-4 lg:row-start-4 lg:row-span-1 lg:pl-0 z-40 col-span-4 col-start-1 row-start-5 flex h-8 items-center justify-start pl-[20%] font-spaceMono text-adobe-white">
+        <div className="z-40 col-start-4 row-span-1 row-start-5 h-8 w-1/2 rounded-br-xl bg-adobe-red xs:w-1/4 sm:w-1/4 lg:hidden"></div>
+        <div className="text-mobile-tagline z-40 col-span-3 col-start-1 row-start-5 flex h-8 items-center justify-start bg-adobe-red pl-[20%] font-spaceMono text-adobe-white lg:absolute lg:bottom-[1.15rem] lg:right-11 lg:col-span-4 lg:col-start-1 lg:row-span-1 lg:row-start-4 lg:h-1/4 lg:w-[75%] lg:rounded-tl-xl"></div>
+        <div className="text-mobile-tagline z-40 col-span-4 col-start-1 row-start-5 flex h-8 items-center justify-start pl-[20%] font-spaceMono text-adobe-white lg:absolute lg:bottom-[1.15rem] lg:right-11 lg:col-span-4 lg:col-start-1 lg:row-span-1 lg:row-start-4 lg:h-1/4 lg:w-[75%] lg:justify-center lg:pl-0">
           {Data.heroPageTagline}
         </div>
         {/* BOTTOM RED BOX */}
-        <div className="lg:absolute lg:right-0 lg:col-start-2 lg:col-span-2 lg:rounded-tl-xl lg:w-[90%] lg:h-[8%] lg:row-start-5 col-span-1 col-start-1 row-span-1 row-start-5 ml-auto h-full w-1/4 bg-adobe-red"></div>
-        <div className="lg:col-start-2 lg:col-span-2 lg:w-[70%] lg:h-[8%] lg:top-6 lg:right-0 lg:absolute lg:row-start-5 col-span-1 col-start-1 row-span-1 row-start-6 ml-auto h-1/2 w-1/4 bg-adobe-red"></div>
-        <div className="lg:col-start-2 lg:right-0 lg:col-span-2 lg:w-[50%] lg:h-[8%] lg:rounded-bl-xl lg:row-start-5 lg:absolute lg:top-12 col-span-1 col-start-2 row-span-1 row-start-6 h-1/2 w-full bg-adobe-red"></div>
+        <div className="col-span-1 col-start-3 row-span-1 row-start-6 ml-auto h-[45%] w-full bg-adobe-red lg:absolute lg:right-0 lg:col-span-2 lg:col-start-2 lg:row-start-5 lg:h-[8%] lg:w-[90%] lg:rounded-tl-xl"></div>
+        <div className="col-span-1 col-start-1 row-span-1 row-start-6 ml-auto h-[45%] w-[10%] bg-adobe-red lg:absolute lg:right-0 lg:top-6 lg:col-span-2 lg:col-start-2 lg:row-start-5 lg:h-[8%] lg:w-[70%]"></div>
+        <div className="col-span-1 col-start-2 row-span-1 row-start-6 h-[45%] w-full bg-adobe-red lg:absolute lg:right-0 lg:top-12 lg:col-span-2 lg:col-start-2 lg:row-start-5 lg:h-[8%] lg:w-[50%] lg:rounded-bl-xl"></div>
       </div>
     </div>
   );
